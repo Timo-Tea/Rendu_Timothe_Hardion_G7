57d1dec144641e9e0b74792ee22867c1e0b449d2
Timo-Tea <115234108+Timo-Tea@users.noreply.github.com>
2023-01-22 23:45:29 +0100

    Add files via upload

---
+++ Rendu.js
@@ -0,0 +1,70 @@
+const heure = document.querySelector("#heure");
+
+setInterval(() => {
+  const date = new Date();
+  heure.innerHTML =
+    (date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()) +
+    ":" +
+    (date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()) +
+    ":" +
+    (date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds());
+}, 1000);
+
+heure.classList.add("bleu");
+
+window.addEventListener("keydown", (event) => {
+  const top = window.getComputedStyle(Blitzø).top;
+  if (event.key === "z") {
+    Blitzø.style.top = `${parseInt(top) - 20}px`;
+  } else if (event.key === "s") {
+    Blitzø.style.top = `${parseInt(top) + 20}px`;
+  }
+});
+
+let i = 0;
+const Blitzø = document.querySelector(".Blitzø");
+const interval = setInterval(() => {
+  Blitzø.style.transform = `translatex(${i}px)`;
+  i = i + 1;
+  if (i >= window.innerWidth) {
+    i = -100;
+    //ou "i=0;"
+    //c'est pour faire apparaitre le gif tout a gauche et un peu cacher
+  }
+});
+
+Blitzø.addEventListener("mouseover", () => {
+  const y = Math.floor(Math.random() * window.innerHeight);
+  Blitzø.style.top = `${y}px`;
+});
+
+const button = document.querySelector("bouton");
+const ici = document.querySelector(".ici");
+
+button.addEventListener("click", function () {
+
+  const y = Math.floor(Math.random() * window.innerHeight);
+  
+  ici.style.top = `${y}px`;
+});
+
+
+
+// const bouton = document.querySelector("#bouton");
+
+// bouton.addEventListener("click", () => {
+//   // alert("Bouton cliqué");
+//   const x =Math.floor(Math.random() * window.innerWidth);
+//   const y =Math.floor(Math.random() * window.innerHeight);
+//   Blitzø.style.left = `${x}px`;
+//   Blitzø.style.top = `${y}px`;
+
+// });
+
+// setInterval(() => {
+//   document.body.style.backgroundColor = `rgb(
+//     ${Math.floor(Math.random() * 255)},
+//     ${Math.floor(Math.random() * 255)},
+//     ${Math.floor(Math.random() * 255)}
+//   )`;
+// }, 100);
